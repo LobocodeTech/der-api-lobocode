a8b6729c30fab984af57cfae8338b904f77c2018
José Henrique <josehenriquefnbr@gmail.com>
2026-05-20 14:13:38 -0300

    feat: inserção campos de edicula na model da Localidade, opcionais, para quando a edicula for true eles estiverem preenchidos

--- src/modules/locations/dto/create-locations.dto.ts
+++ src/modules/locations/dto/create-locations.dto.ts
@@ -31,6 +31,31 @@ export class CreateLocationsDto {
   @IsBoolean({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
   hasEdicule?: boolean;
 
+  @IsOptional()
+  @IsString({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
+  @MaxLength(8, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
+  ediculePostalCode?: string | null;
+
+  @IsOptional()
+  @IsString({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
+  @MaxLength(200, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
+  ediculeStreet?: string | null;
+
+  @IsOptional()
+  @IsString({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
+  @MaxLength(20, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
+  ediculeNumber?: string | null;
+
+  @IsOptional()
+  @IsString({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
+  @MaxLength(120, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
+  ediculeCity?: string | null;
+
+  @IsOptional()
+  @IsString({ message: VALIDATION_MESSAGES.FORMAT.FIELD_INVALID })
+  @MaxLength(200, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
+  ediculeLegalName?: string | null;
+
   @IsString({ message: VALIDATION_MESSAGES.REQUIRED.NAME })
   @MaxLength(120, { message: VALIDATION_MESSAGES.LENGTH.MAX_LENGTH })
   name: string;
